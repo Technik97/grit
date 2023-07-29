7dcd0f6b4892101cad53621fb64e6045060f59a9
TechNik <aniket.97.mane@gmail.com>
2023-07-29 23:57:10 +0530

    Created Habit entity file

---
+++ Grit.Database/Entities/Habit.cs
@@ -0,0 +1,10 @@
+namespace Grit.Database.Entities;
+
+public class Habit
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+}
