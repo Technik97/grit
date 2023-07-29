b738cea14b9044355306f8587df7fffe077564ca
TechNik <aniket.97.mane@gmail.com>
2023-07-30 01:40:39 +0530

    Added required services

--- Grit.HabitService/Program.cs
+++ Grit.HabitService/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+using Grit.HabitService.Interfaces;
+using Grit.HabitService.Services;
+using Grit.Database;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var dsBuilder = new NpgsqlDataSourceBuilder(builder.Configuration.GetConnectionString("Postgres"));
+var dbDataSource = dsBuilder.Build();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddTransient<IHabitService, HabitService>();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<GritDbContext>(options =>
+{
+    options.UseNpgsql(dbDataSource);
+});
 
 var app = builder.Build();
 
