95b5c410a92baf41dea4c4033191e33aa5902235
TechNik <aniket.97.mane@gmail.com>
2023-08-02 23:36:32 +0530

    Added HabitDto

---
+++ Grit.HabitService/Dtos/HabitDto.cs
@@ -0,0 +1,10 @@
+namespace Grit.HabitService.Dtos;
+
+public class HabitDto
+{
+    public int Id { get; set; } = default!;
+
+    public string Name { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+}
